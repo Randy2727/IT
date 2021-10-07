295d7f63b5a4324478307dc4d899e18679a9e622
Randy2727 <19766404+Randy2727@users.noreply.github.com>
2021-10-07 09:55:38 -0700

    Update Modify calendar permissions.ps1
    Changed -Set-MailboxFolderPermission to Add-MailboxFolderPermission
    
    Updated comments to clarify functionality
    
    Changed "mailbox" to "calendar" in user prompts

--- Powershell/Modify calendar permissions.ps1
+++ Powershell/Modify calendar permissions.ps1
@@ -1,13 +1,15 @@
 <#
-	Grants or modifies a user's access to another user's calendar, returns all permissions for user's calendar, waits 20 seconds, then terminates the session.
+	Grants a user access to another user's calendar, returns all permissions for user's calendar, waits 20 seconds, then terminates the session.
+
+    This will not modify an existing permission, only add a new one
 
     Must be connected to ExchangeOnline module as an Exchange administrator or higher
 #>
 
 connect-exchangeonline
 
-$User1 = Read-Host -Prompt "Enter email address of user whose mailbox is being shared"
-$User2 = Read-Host -Prompt "Enter email address of user is being given access to mailbox"
+$User1 = Read-Host -Prompt "Enter email address of user whose calendar is being shared"
+$User2 = Read-Host -Prompt "Enter email address of user is being given access to calendar"
 $PermissionLevel = Read-Host -Prompt "Enter permission level being granted"
 
 <#
@@ -25,8 +27,8 @@ $PermissionLevel = Read-Host -Prompt "Enter permission level being granted"
     None — no permissions to access folder and files.
 #>
 
-Set-MailboxFolderPermission -Identity ${User1}:\calendar -User $User2 -AccessRights $PermissionLevel
-#This may need to be changed back to Add-MailboxFolderPermission
+Add-MailboxFolderPermission -Identity ${User1}:\calendar -User $User2 -AccessRights $PermissionLevel
+#Set-MailboxFolderPermission for changing an existing permission
 
 Start-Sleep -s 2
 
