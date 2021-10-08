cb02844b4df16765ffa5c80c7bfe6e6492a793bf
Randy2727 <19766404+Randy2727@users.noreply.github.com>
2021-10-07 17:09:55 -0700

    Create Zoom - Disable auto update.ps1

---
+++ Registry/Zoom - Disable auto update.ps1
@@ -0,0 +1,13 @@
+﻿New-Item -Path 'Registry::HKCU\SOFTWARE\Policies\Zoom'
+
+Start-Sleep -s 3
+
+New-Item -Path 'Registry::HKCU\SOFTWARE\Policies\Zoom\Zoom Meetings'
+
+Start-Sleep -s 3
+
+New-Item -Path 'Registry::HKCU\SOFTWARE\Policies\Zoom\Zoom Meetings\General'
+
+Start-Sleep -s 3
+
+New-ItemProperty -Path 'HKCU:\SOFTWARE\Policies\Zoom\Zoom Meetings\General' -Name EnableClientAutoUpdate -PropertyType DWord -Value $0
